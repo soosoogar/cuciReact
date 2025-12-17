1a00ac9402c72c583f3d41b05d8f51e80e012dac
KHS <khsoo8546@gmail.com>
2025-12-17 13:35:37 +0900

    학생 정보 리스트 화면 추가

--- src/page/StudentInfo.jsx
+++ src/page/StudentInfo.jsx
@@ -1,4 +1,4 @@
-
+import stu from '../data/student.json'; 
 /**
  * 
  *  학생 정보 불러오기 json
@@ -7,6 +7,28 @@ export default function StudentInfo() {
   return (
     <>
       <h1>Student Info Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>이름</th>
+            <th>나이</th>
+            <th>이메일</th>
+            <th>지역</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stu.students.map((stu,index)=>{
+            return (
+              <tr key={index}>
+                <td>{stu.name}</td>
+                <td>{stu.age}</td>
+                <td>{stu.email}</td>
+                <td>{stu.region}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </>
   )
 }
