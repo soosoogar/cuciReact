8b227df658082ae68108193e2d9ef6ab2f084af4
KHS <khsoo8546@gmail.com>
2025-12-17 13:21:06 +0900

    학생정보 입력 페이지 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Main from './page/Main.jsx';
 import Calc from './page/Calc.jsx';
 import Dark from './page/Dark.jsx';
+import StudentInfo from './page/StudentInfo.jsx';
 
 function App() {
 
@@ -11,8 +12,9 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path={'/'} element={<Main />} />
-        <Route path={'/Calc'} element={<Calc />} />
-        <Route path={'/Dark'} element={<Dark />} />
+        <Route path={'/calc'} element={<Calc />} />
+        <Route path={'/dark'} element={<Dark />} />
+        <Route path={'/studentinfo'} element={<StudentInfo />} />
       </Routes>
       </BrowserRouter>
     </>
---
+++ src/page/StudentInfo.jsx
@@ -0,0 +1,12 @@
+
+/**
+ * 
+ *  학생 정보 불러오기 json
+ */
+export default function StudentInfo() {
+  return (
+    <>
+      <h1>Student Info Page</h1>
+    </>
+  )
+}
